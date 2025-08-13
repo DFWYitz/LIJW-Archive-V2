0f7b15777742792c88bc6fe488402caff414c189
yitz <feigenbaumsfish@gmail.com>
2025-08-12 20:27:23 -0500

    updated script

---
+++ script.js
@@ -0,0 +1,18 @@
+const publications = [
+  { id: 'ODg8898151', 
+    title: 'The Big Lie - The Genocide Libel Refuted', 
+    date: '8-1-25', 
+    tags: ['I/P Conflict', 'Debunking Propaganda',] 
+    featured: true  }
+];
+
+function loadPublications(){
+
+}
+
+function filterByMonth(){
+
+}
+document.addEventListener('DOMContentLoaded', function(){
+  loadPublications();
+});
